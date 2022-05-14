a565e9b5f1c076ebcfda6234700b723e204bcafc
Anthony Rose <20302208+Cx01N@users.noreply.github.com>
2022-05-13 18:11:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pyVNC',    # This is the name of your PyPI-package.
     version='0.1',                          # Update the version number for new releases
-    install_requires=['twisted', 'numpy', 'pygame', 'service-identity'],
+    install_requires=['twisted', 'numpy', 'pygame', 'service_identity'],
     packages=["pyVNC"],
     scripts=['./pyVNC/pyvnc.py']                  # The name of your scipt, and also the command you'll be using for calling it
 )
